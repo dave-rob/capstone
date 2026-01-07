beb378cecc693309aeca0e46211238c462184293
Dave <dlrobjr@gmail.com>
2026-01-07 15:14:21 -0700

    Implemented ingest_results.py v1, ingests raw results.csv and creates parquet files partitioned by the year

---
+++ scripts/ingest_results.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Wrangling Quiz").getOrCreate()
+
+path = "./data/raw/Results.csv"
+full_df = spark.read.csv(path, header=True)
+
+#full_df.printSchema()
+
+#drop name and zip code
+df = full_df.drop("Name", "Zip")
+
+#df.show(5)
+
+output_path = "data/staging/results"
+
+df.write.mode("append").partitionBy("year").parquet(output_path)
