5e6be63b908743a470d46db86c6d157645de90a2
Dave <dlrobjr@gmail.com>
2026-01-09 12:36:39 -0700

    updated clean_results.py

--- scripts/spark_jobs/clean_results.py
+++ scripts/spark_jobs/clean_results.py
@@ -15,9 +15,12 @@ null_counts.show()
 missing_age_df=df.select("*").where(col("Age Group").isNull())
 missing_age_df.describe().show()
 
+df = df.drop("Country")
+
 #choose rows where there the Age Group column is not Null due to max Age being 1 in those rows
 final_df = df.where(col("Age Group").isNotNull())
 print(final_df.count())
+final_df.printSchema()
 
 output_path = f"/opt/airflow/data/staging/Results"
 
