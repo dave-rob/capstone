111a0428531171ca3ad9fe7c88e817e0bc6d9595
Dave <dlrobjr@gmail.com>
2026-01-09 14:38:32 -0700

    updated cleaning results script

--- scripts/spark_jobs/clean_results.py
+++ scripts/spark_jobs/clean_results.py
@@ -1,7 +1,23 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum
+from pyspark.sql import functions as F
+from pyspark.sql.types import NumericType
 
 spark = SparkSession.builder.appName("CleanResults").getOrCreate()
+
+def detect_iqr_outliers(df, col):
+
+    # Compute quartiles
+    Q1, Q3 = df.approxQuantile(col, [0.25, 0.75], 0.01)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+
+    print(f'The lower limit is: {lower}\nThe upper limit is: {upper}')
+    print(f'Dropping {df.where(F.col(col) < lower).count()} runners for being below the lower limit')
+    print(f'Dropping {df.where(F.col(col) > upper).count()} runners for being above the upper limit')
+    return  df.where((F.col(col) > lower) & (F.col(col) < upper))
+    
 
 df = spark.read.csv("data/staging/Results", header=True)
 
@@ -18,9 +34,15 @@ missing_age_df.describe().show()
 df = df.drop("Country")
 
 #choose rows where there the Age Group column is not Null due to max Age being 1 in those rows
-final_df = df.where(col("Age Group").isNotNull())
+df = df.where(col("Age Group").isNotNull())
+
+df_casted = df.withColumn("Finish", col("Finish").cast("int"))
+
+no_outlier_df = detect_iqr_outliers(df_casted, "Finish")
+
+# fastest marathon time is 1:59:40 or 7180 seconds
+final_df = no_outlier_df.where(col("Finish") >= 7180)
 print(final_df.count())
-final_df.printSchema()
 
 output_path = f"/opt/airflow/data/staging/Results"
 
