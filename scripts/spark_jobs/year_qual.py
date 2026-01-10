ae9ec2ba335c5be5f131969a515d47e44e077dd9
Dave <dlrobjr@gmail.com>
2026-01-09 21:04:34 -0700

    added aggregate showing qualifiers based on new or old BQ standards

---
+++ scripts/spark_jobs/year_qual.py
@@ -0,0 +1,31 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count, sum, col, asc
+from pyspark.sql.types import IntegerType
+
+spark = SparkSession.builder.appName("YearQual").getOrCreate()
+
+df = spark.read.parquet("data/curated/Qualified_Results")
+df = df.withColumn("Qualified", col("Qualified").cast("boolean"))
+
+df.printSchema()
+df.show(10)
+
+df_encoded = df.withColumn("Qualified_encoded", col("Qualified").cast(IntegerType()))
+df_encoded.show(10)
+
+qual_rate_df = df_encoded.groupBy(
+    "Year", "2026_BQ_Entry"
+).agg(
+    count("*").alias("total_runners"),
+    sum(col("Qualified_encoded")).alias("qualified_runners")
+).orderBy(
+    asc('Year')
+)
+
+qual_rate_df.show()
+
+output_path = f"/opt/airflow/data/aggregates/YearQual"
+
+qual_rate_df.write.mode("overwrite").parquet(output_path)
+
+spark.stop()
